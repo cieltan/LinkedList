8e0aa44ab9d1e5b0e573a8758a983d1df2a09abd
btan <btan7645@gmail.com>
2020-03-05 19:21:40 -0500

    Initial commit

---
+++ src/MyLinkedList.java
@@ -0,0 +1,61 @@
+public class MyLinkedList implements NodeList {
+
+    private ListItem root;
+
+    public MyLinkedList(ListItem root) {
+        this.root = root;
+    }
+
+    @Override
+    public ListItem getRoot() {
+        return this.root;
+    }
+
+    @Override
+    public boolean addItem(ListItem newItem) {
+        if (this.root == null) {
+            //The list was empty
+            this.root = newItem;
+            return true;
+        }
+
+        ListItem currentItem = this.root;
+        while (currentItem != null) {
+            int comparison = (currentItem.compareTo(newItem));
+            if (comparison < 0) {
+                if (currentItem.next() != null) {
+                    currentItem = currentItem.next();
+                } else {
+                    currentItem.setNext(newItem);
+                    newItem.setPrevious(currentItem);
+                    return true;
+                }
+            } else if (comparison > 0) {
+                if (currentItem.previous() != null) {
+                    currentItem.previous().setNext(newItem);
+                    newItem.setPrevious(currentItem.previous());
+                    newItem.setNext(currentItem);
+                    currentItem.setPrevious(currentItem);
+                } else {
+                    newItem.setNext(this.root);
+                    this.root.setPrevious(newItem);
+                    this.root = newItem;
+                }
+            } else {
+                System.out.println(newItem.getValue() + " is already present.");
+                return false;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean removeItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public void traverse(ListItem root) {
+
+    }
+}
